2f3bfba2b44437c1af4f06ffb377d7869248548f
kingsli <kingoyelabi@gmail.com>
2024-06-14 01:19:05 +0100

    base site structure and styles

---
+++ src/components/Email.ts
@@ -0,0 +1,7 @@
+import { links } from "./links";
+
+export const MAIL = links.email;
+
+export const SUBJECT = "Hey Kingsley, I'd like to work with you";
+export const BODY =
+    "Hi Kingsley, I came across your website and I think it would be great to work with you. Can we schedule a meeting to discuss further?";
---
+++ src/components/nav.ts
@@ -0,0 +1,44 @@
+import type { icon_paths } from "./icon_paths";
+
+import { links } from "./links";
+
+export const nav_menu_links: { label: string; href: string }[] = [
+    { label: "Home", href: "/" },
+    { label: "Projects", href: "/projects/" },
+    { label: "About", href: "/about/" },
+];
+
+export const nav_social_links: {
+    label: string;
+    href: string;
+    icon: keyof typeof icon_paths;
+}[] = [
+    { label: "GitHub", href: links.github, icon: "github-logo" },
+    {
+        label: "Stack Overflow",
+        href: links.stack_overflow,
+        icon: "stack-overflow",
+    },
+    { label: "Email", href: `mailto:${links.email}`, icon: "gmail" },
+    { label: "Twitter", href: links.twitter, icon: "twitter-logo" },
+    { label: "LinkedIn", href: links.linkedin, icon: "linkedin-logo" },
+    // {label: "Resume", href: links.resume, icon: "resume"},
+];
+
+export const resume_link: {
+    label: string;
+    href: string;
+    icon: "resume"
+} = {
+    label: "Resume",
+    href: links.resume,
+    icon: "resume",
+};
+
+export function is_nav_link_active(current_url: string, href: string) {
+    return (
+        current_url === href ||
+        (href !== "/" && current_url.startsWith(href)) ||
+        (href === "/projects/" && current_url.startsWith("/project"))
+    );
+}
