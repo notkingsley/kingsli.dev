0633f1072ece98ab247928453912b9ee2dd74545
kingsli <kingoyelabi@gmail.com>
2024-06-16 03:24:36 +0100

    Zod schema validation for project content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
-	work: defineCollection({
+	projects: defineCollection({
 		type: 'content',
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
-			publishDate: z.coerce.date(),
 			tags: z.array(z.string()),
 			img: z.string(),
 			img_alt: z.string().optional(),
+			source: z.string().optional(),
+			site: z.string().optional(),
+			content: z.string(),
+			order: z.number(),
 		}),
 	}),
 };
