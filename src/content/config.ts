7cf663d33e9489d5e8aa1001cc4d9a26e143c346
kingsli <kingoyelabi@gmail.com>
2024-06-18 05:48:22 +0100

    define work collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,30 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
-	projects: defineCollection({
-		type: 'content',
-		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
-			source: z.string().optional(),
-			site: z.string().optional(),
-			content: z.string(),
-			order: z.number(),
-		}),
-	}),
+    projects: defineCollection({
+        type: "content",
+        schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            tags: z.array(z.string()),
+            img: z.string(),
+            img_alt: z.string().optional(),
+            source: z.string().optional(),
+            site: z.string().optional(),
+            content: z.string(),
+            order: z.number(),
+        }),
+    }),
+    work: defineCollection({
+        type: "content",
+        schema: z.object({
+            position: z.string(),
+            type: z.string(),
+            company: z.string(),
+            link: z.string(),
+            date: z.string(),
+            activities: z.array(z.string()),
+            order: z.number(),
+        }),
+    }),
 };
