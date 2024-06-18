7d50d22cee7ba0bd123a1ec906c3114d3f0cf1de
kingsli <kingoyelabi@gmail.com>
2024-06-18 05:57:20 +0100

    add work page to nav

--- src/components/nav.ts
+++ src/components/nav.ts
@@ -6,6 +6,7 @@ export const nav_menu_links: { label: string; href: string }[] = [
     { label: "Home", href: "/" },
     { label: "Projects", href: "/projects/" },
     { label: "About", href: "/about/" },
+    { label: "Work", href: "/work" },
     { label: "Resume", href: "/resume" },
 ];
 
