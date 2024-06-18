a187d266710286b8678aa038696ce6a98b60a991
kingsli <kingoyelabi@gmail.com>
2024-06-18 21:09:46 +0100

    define old-projects collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -27,6 +27,20 @@ export const collections = {
             order: z.number(),
         }),
     }),
+    old_projects: defineCollection({
+        type: "data",
+        schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            tags: z.array(z.string()),
+            img: z.string(),
+            img_alt: z.string().optional(),
+            source: z.string().optional(),
+            site: z.string().optional(),
+            content: z.string(),
+            order: z.number(),
+        }),
+    }),
     old_work: defineCollection({
         type: "data",
         schema: z.object({
