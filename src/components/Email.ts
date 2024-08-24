2d7568bf410fafedcfe1a0fd3a285da197a7abee
kingsli <kingoyelabi@gmail.com>
2024-08-24 22:18:06 +0100

    moved contact email into config

--- src/components/Email.ts
+++
@@ -1,7 +0,0 @@
-import { links } from "./config";
-
-export const MAIL = links.email;
-
-export const SUBJECT = "Hey Kingsley, I'd like to work with you";
-export const BODY =
-    "Hi Kingsley, I came across your website and I think it would be great to work with you. Can we schedule a meeting to discuss further?";
--- src/components/config.ts
+++ src/components/config.ts
@@ -8,3 +8,8 @@ export const links = {
     email: "kingsley.not@gmail.com",
     resume_file: "/assets/files/kingsley_24_06.pdf",
 };
+
+export const CONTACT_EMAIL = links.email;
+export const CONTACT_TEMPLATE_SUBJECT = `Hey Kingsley, I'd like to work with you`;
+export const CONTACT_TEMPLATE_BODY = `Hi Kingsley, I came across your website and I think it would be great to work with you.
+Can we schedule a meeting to discuss further?`;
